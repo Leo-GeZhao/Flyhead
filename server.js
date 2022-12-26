3f872b20616ad0bfbb2065f1faf7a35c2fc50df7
Ge Zhao <gezhao@Ges-MacBook-Pro.local>
2022-12-26 11:40:36 -0500

    fix deployment

--- server.js
+++ server.js
@@ -13,11 +13,8 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 
-
-if( process.env.NODE_ENV !== 'development'){
-    app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-    app.use(express.static(path.join(__dirname, 'build')));
-  }
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/api/events', eventRouter)
 
