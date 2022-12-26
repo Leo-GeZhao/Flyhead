d349fe9fe86d1b65e5e226f58e52bf944975efa2
Ge Zhao <gezhao@Ges-MacBook-Pro.local>
2022-12-26 08:08:41 -0500

    fix map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -32,47 +32,47 @@ const Map = ({setCoordinates, setBounds, coordinates, places, setChildClick}) =>
             </Autocomplete>
           </div> 
           <div className="map__container mt-2">
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
-            defaultCenter={coordinates}
-            center={coordinates}
-            defaultZoom={14}
-            margin={[50, 50, 50, 50]}
-            options={''}
-            onChange={(e)=> {
-              setCoordinates({lat:e.center.lat, lng:e.center.lng})
-              setBounds({ne:e.marginBounds.ne, sw:e.marginBounds.sw})
-            }}
-            onChildClick={(child) => setChildClick(child)}
-            >
-            {places?.map((place,i)=>(
-              <div 
-              className="marker__container"
-              lat={Number(place.latitude)}
-              lng={Number(place.longitude)}
-              key={i}
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+              defaultCenter={coordinates}
+              center={coordinates}
+              defaultZoom={14}
+              margin={[50, 50, 50, 50]}
+              options={''}
+              onChange={(e)=> {
+                setCoordinates({lat:e.center.lat, lng:e.center.lng})
+                setBounds({ne:e.marginBounds.ne, sw:e.marginBounds.sw})
+              }}
+              onChildClick={(child) => setChildClick(child)}
               >
-              {
-                !isDesktop?(
-                  <IoLocationOutline/>
-                )
-                :
-                (
-                  <div className='card border-0 px-2'>
-                    <div className='card-subtitle text-center mt-1'>
-                      {place.name}
+              {places?.map((place,i)=>(
+                <div 
+                className="marker__container"
+                lat={Number(place.latitude)}
+                lng={Number(place.longitude)}
+                key={i}
+                >
+                {
+                  !isDesktop?(
+                    <IoLocationOutline/>
+                  )
+                  :
+                  (
+                    <div className='card border-0 px-2'>
+                      <div className='card-subtitle text-center mt-1'>
+                        {place.name}
+                      </div>
+                        <img 
+                        src={place.photo? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'} 
+                        alt={place.name} 
+                        className="card-top-img my-1"/>
+                        <Rating name="read-only" size="small" value={Number(place.rating)} readOnly/>
                     </div>
-                      <img 
-                      src={place.photo? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'} 
-                      alt={place.name} 
-                      className="card-top-img my-1"/>
-                      <Rating name="read-only" size="small" value={Number(place.rating)} readOnly/>
-                  </div>
-                )
-              } 
-              </div>
-            ))}
-          </GoogleMapReact>
+                  )
+                } 
+                </div>
+              ))}
+            </GoogleMapReact>
         </div>
     </div>
   )
